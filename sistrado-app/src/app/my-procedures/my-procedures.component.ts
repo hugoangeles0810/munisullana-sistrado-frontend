d6e7adf73786d26e74cd1fc895361b30bc3da925
Hugo Ángeles <hugoangeles0810@gmail.com>
2021-02-06 17:03:15 -0500

    Added my procedures

--- sistrado-app/src/app/my-procedures/my-procedures.component.ts
+++ sistrado-app/src/app/my-procedures/my-procedures.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-my-procedures',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-procedures.component.css']
 })
 export class MyProceduresComponent implements OnInit {
+
+  displayedColumns: string[] = ['created', 'state', 'number', 'title'];
+  data: any[] = [];
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor() { }
 
