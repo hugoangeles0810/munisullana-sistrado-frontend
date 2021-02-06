9081039be69f30e729c19b33e128738d7aa6a91b
Hugo Ángeles <hugoangeles0810@gmail.com>
2021-02-06 14:50:43 -0500

    Updated login and register

--- sistrado-app/src/app/home/home.component.ts
+++ sistrado-app/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { UploadService } from '../upload.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,27 @@ import { AuthService } from '../auth.service';
 })
 export class HomeComponent implements OnInit {
 
+  fileToUpload: File = null;
+
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private uploadService: UploadService) { }
 
   ngOnInit(): void {
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    this.uploadFileToActivity();
+  }
+
+  uploadFileToActivity() {
+    this.uploadService.postFile(this.fileToUpload).subscribe(data => {
+      // do something, if upload success
+        console.log(data);
+      }, error => {
+        console.log(error);
+      });
   }
 
   logout() {
---
+++ sistrado-app/src/app/upload.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadService {
+
+  constructor(private http: HttpClient,
+    private authService: AuthService) { }
+
+  postFile(fileToUpload: File): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    return this.http
+      .post(`${environment.apiUrl}/webapp/api/v1/solicitud/adjunto/upload`, formData, { headers: { authorization: "Bearer " + this.authService.getToken() }}).pipe();
+}
+}
