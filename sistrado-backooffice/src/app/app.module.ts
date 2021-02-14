b8dcfc6b361fcf11e9c87a120cce0935bb2c7b12
Hugo Ángeles <hugoangeles0810@gmail.com>
2021-02-14 16:44:51 -0500

    Added logued section

--- sistrado-backooffice/src/app/app.module.ts
+++ sistrado-backooffice/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
